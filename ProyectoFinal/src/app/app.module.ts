f99e082a4357401259da0b96e984c695f1092e6d
magustinaperlo <43002670+magustinaperlo@users.noreply.github.com>
2022-03-31 21:27:07 -0300

    skills

--- ProyectoFinal/src/app/app.module.ts
+++ ProyectoFinal/src/app/app.module.ts
@@ -10,6 +10,10 @@ import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
+
 
 @NgModule({
   declarations: [
@@ -20,11 +24,14 @@ import { EducacionComponent } from './components/educacion/educacion.component';
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
-    EducacionComponent
+    EducacionComponent,
+    ProyectoComponent,
+    HysComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
