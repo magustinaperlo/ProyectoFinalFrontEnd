296dda42753da2353ec9f18eee3f669343ba2645
magustinaperlo <43002670+magustinaperlo@users.noreply.github.com>
2022-10-30 10:59:10 -0300

    Subo previo a deploy

---
+++ ProyectoFinal/BackEnd/maa/src/main/java/com/portfolio/maa/MaaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.maa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaaApplication.class, args);
+	}
+
+}
